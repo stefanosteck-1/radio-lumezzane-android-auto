4132f4bf3d46275f5785b085460f1026498c45b6
stefanosteck <stefanosteck@icloud.com>
2026-09-14 17:09:16 +0200

    chore: initialize Android + Android Auto starter

---
+++ app/src/main/java/it/radiolumezzane/auto/MainActivity.kt
@@ -0,0 +1,8 @@
+package it.radiolumezzane.auto
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+class MainActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+  }
+}
